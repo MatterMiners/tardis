788afe7ce074cfa8e30db392c0508e1776092076
Manuel Giffels <giffels@gmail.com>
2018-08-15 18:09:30 +0200

    Avoid Unknown child returncode, potential python bug

--- tardis/utilities/asynccachemap.py
+++ tardis/utilities/asynccachemap.py
@@ -1,6 +1,9 @@
 from collections.abc import Mapping
 from time import time
 
+import asyncio
+import json
+
 
 class AsyncCacheMap(Mapping):
     def __init__(self, update_coroutine, max_age: int= 60 * 15):
@@ -8,12 +11,19 @@ class AsyncCacheMap(Mapping):
         self._max_age = max_age
         self._last_update = 0
         self._data = {}
+        self._lock = asyncio.Lock()
 
     async def update_status(self):
         current_time = time()
-        if (current_time - self._last_update) > self._max_age:
-            self._data = await self._update_coroutine()
-            self._last_update = current_time
+        async with self._lock:
+            if (current_time - self._last_update) > self._max_age:
+                    try:
+                        data = await self._update_coroutine()
+                    except json.decoder.JSONDecodeError:
+                        pass
+                    else:
+                        self._data = data
+                    self._last_update = current_time
 
     def __iter__(self):
         return iter(self._data)
--- tardis/utilities/utils.py
+++ tardis/utilities/utils.py
@@ -9,7 +9,11 @@ async def async_run_command(cmd, *args):
 
     stdout, stderr = await sub_process.communicate()
 
-    if not sub_process.returncode:
+    # Potentially due to a Python bug, if waitpid(0) is called somewhere else, the message
+    # "WARNING:asyncio:Unknown child process pid 2960761, will report returncode 255 appears"
+    # However the command succeeded
+
+    if sub_process.returncode in (0, 255):
         return stdout.decode().strip()
 
     raise AsyncRunCommandFailure(message=stdout, error_code=sub_process.returncode, error_message=stderr)
