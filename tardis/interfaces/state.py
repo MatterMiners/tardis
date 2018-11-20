a8ceaadbe8ed3bd17e114b7cd7b7fbc7bde95da2
Manuel Giffels <giffels@gmail.com>
2018-11-20 06:02:55 +0100

    Add state of resources to registry

--- tardis/interfaces/state.py
+++ tardis/interfaces/state.py
@@ -10,6 +10,10 @@ class State(metaclass=ABCMeta):
     def __repr__(self):
         return self.__class__.__name__
 
+    @classmethod
+    def get_all_states(cls):
+        return [subclass.__name__ for subclass in cls.__subclasses__()]
+
     @staticmethod
     @abstractmethod
     async def run(drone):
