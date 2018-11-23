fa387896d0b329d6a78c4a5b64d0323e2751fb4f
Manuel Giffels <giffels@gmail.com>
2018-11-23 04:29:00 +0100

    Use sqlite named bindings

--- tardis/observers/sqliteregistry.py
+++ tardis/observers/sqliteregistry.py
@@ -59,25 +59,24 @@ class SqliteRegistry(Observer):
 
     async def notify(self, state, resource_attributes):
         logging.debug(f"Drone: {str(resource_attributes)} has changed state to {state}")
+        bind_parameters = dict(state=str(state))
+        bind_parameters.update(resource_attributes)
+
         async with self.connect(flavour=aiosqlite) as connection:
             async with connection.cursor() as cursor:
                 try:
                     await cursor.execute("BEGIN TRANSACTION")
                     await cursor.execute("""INSERT OR IGNORE INTO 
                     Resources(resource_id, dns_name, state_id, site_id, machine_type_id, created, updated) 
-                    SELECT ?, ?, RS.state_id, S.site_id, MT.machine_type_id, ?, ?
+                    SELECT :resource_id, :dns_name, RS.state_id, S.site_id, MT.machine_type_id, :created, :updated
                     FROM ResourceStates RS
-                    JOIN Sites S ON S.site_name = ?
-                    JOIN MachineTypes MT ON MT.machine_type = ? AND MT.site_id = S.site_id
-                    WHERE RS.state = ?""", (resource_attributes['resource_id'], resource_attributes['dns_name'],
-                                            resource_attributes['created'], resource_attributes['updated'],
-                                            resource_attributes['site_name'], resource_attributes['machine_type'],
-                                            str(state)))
-                    await cursor.execute("""UPDATE Resources SET updated = ?,
-                    state_id = (SELECT state_id FROM ResourceStates WHERE state = ?) 
-                    WHERE resource_id = ? AND site_id = (SELECT site_id FROM Sites WHERE site_name = ?)""",
-                                         (resource_attributes['updated'], str(state),
-                                          resource_attributes['resource_id'], resource_attributes['site_name']))
+                    JOIN Sites S ON S.site_name = :site_name
+                    JOIN MachineTypes MT ON MT.machine_type = :machine_type AND MT.site_id = S.site_id
+                    WHERE RS.state = :state""", bind_parameters)
+                    await cursor.execute("""UPDATE Resources SET updated = :updated,
+                    state_id = (SELECT state_id FROM ResourceStates WHERE state = :state) 
+                    WHERE resource_id = :resource_id 
+                    AND site_id = (SELECT site_id FROM Sites WHERE site_name = :site_name)""", bind_parameters)
                 except aiosqlite.Error as error:
                     await cursor.execute("ROLLBACK")
                     raise error
