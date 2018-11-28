ade6dd0e98889305d417115bb45e6cf0d9ae14d7
Manuel Giffels <giffels@gmail.com>
2018-11-28 14:16:50 +0100

    Add unittest for async_run_command

--- tests/utilities_t/test_utils.py
+++ tests/utilities_t/test_utils.py
@@ -1,7 +1,18 @@
 from tardis.utilities.utils import async_run_command
+from tardis.exceptions.tardisexceptions import AsyncRunCommandFailure
 
 from unittest import TestCase
+import asyncio
 
 
 class TestAsyncRunCommand(TestCase):
-    pass
+    def test_async_run_command(self):
+        loop = asyncio.get_event_loop()
+
+        loop.run_until_complete(async_run_command('exit 0'))
+        loop.run_until_complete(async_run_command('exit 255'))
+
+        with self.assertRaises(AsyncRunCommandFailure):
+            loop.run_until_complete(async_run_command('exit 1'))
+
+        self.assertEqual(loop.run_until_complete(async_run_command('echo "Test"')), "Test")
