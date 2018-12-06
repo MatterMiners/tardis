d3896b27bf92bff16f01b766997a8a402346e901
Manuel Giffels <giffels@gmail.com>
2018-12-06 22:43:11 +0100

    Restore previous state after restart

--- tests/resources_t/test_poolfactory.py
+++ tests/resources_t/test_poolfactory.py
@@ -1,7 +1,13 @@
+from tardis.resources.dronestates import RequestState
 from tardis.resources.poolfactory import create_composite_pool
+from tardis.resources.poolfactory import str_to_state
 
 from unittest import TestCase
 
 
 class TestCompositePool(TestCase):
-    pass
+    def test_str_to_state(self):
+        test = [{'state': 'RequestState', 'dns_name': 'test-abc123'}]
+        converted_test = str_to_state(test)
+        self.assertTrue(converted_test[0]['state'], RequestState)
+        self.assertEqual(converted_test[0]['dns_name'], 'test-abc123')
