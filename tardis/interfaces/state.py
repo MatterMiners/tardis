d3fd9352fc09a6354294010c54ed99d1d46ce59c
Manuel Giffels <giffels@gmail.com>
2019-02-06 11:24:45 +0100

    Remove code duplication with pipeline.py

--- tardis/interfaces/state.py
+++ tardis/interfaces/state.py
@@ -1,5 +1,4 @@
 from ..utilities.pipeline import PipelineProcessor
-from ..utilities.pipeline import StopProcessing
 
 from abc import ABCMeta, abstractmethod
 
@@ -25,8 +24,5 @@ class State(metaclass=ABCMeta):
 
     @classmethod
     async def run_processing_pipeline(cls, drone):
-        try:
-            pipeline_processor = PipelineProcessor(cls.processing_pipeline)
-            return await pipeline_processor.run_pipeline(pipeline_input=cls.transition, drone=drone, current_state=cls)
-        except StopProcessing as ex:
-            return ex.last_result
+        pipeline_processor = PipelineProcessor(cls.processing_pipeline)
+        return await pipeline_processor.run_pipeline(pipeline_input=cls.transition, drone=drone, current_state=cls)
