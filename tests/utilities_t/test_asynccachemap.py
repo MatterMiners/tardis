482f4a6ded8ac9cef239646fd24531363faf056e
Manuel Giffels <giffels@gmail.com>
2019-03-13 15:51:24 +0100

    Do not update asynccachemap when command execution failed

--- tests/utilities_t/test_asynccachemap.py
+++ tests/utilities_t/test_asynccachemap.py
@@ -1,25 +1,32 @@
+from tardis.exceptions.executorexceptions import CommandExecutionFailure
 from tardis.utilities.asynccachemap import AsyncCacheMap
+
+from ..utilities.utilities import run_async
 
 from json.decoder import JSONDecodeError
 from unittest import TestCase
-import asyncio
+
+import logging
 
 
 class TestAsyncCacheMap(TestCase):
     def setUp(self):
         self.test_data = {'testA': 123, 'testB': 'Random String'}
         self.async_cache_map = AsyncCacheMap(update_coroutine=self.update_function)
-        self.failing_async_cache_map = AsyncCacheMap(update_coroutine=self.failing_update_function)
+        self.json_failing_async_cache_map = AsyncCacheMap(update_coroutine=self.json_failing_update_function)
+        self.command_failing_async_cache_map = AsyncCacheMap(update_coroutine=self.command_failing_update_function)
 
-    async def failing_update_function(self):
+    async def json_failing_update_function(self):
         raise JSONDecodeError(msg='Bla', doc='Blubb', pos=99)
+
+    async def command_failing_update_function(self):
+        raise CommandExecutionFailure(message='Failure', stdout='Failure', stderr='Failure', exit_code=2)
 
     async def update_function(self):
         return self.test_data
 
     def update_status(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.async_cache_map.update_status())
+        run_async(self.async_cache_map.update_status)
 
     def test_update_async_cache_map(self):
         self.update_status()
@@ -39,7 +46,12 @@ class TestAsyncCacheMap(TestCase):
         for key, value in self.async_cache_map.items():
             self.assertEqual(value, self.test_data.get(key))
 
-    def test_failing_update(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.failing_async_cache_map.update_status())
-        self.assertEqual(len(self.failing_async_cache_map), 0)
+    def test_json_failing_update(self):
+        with self.assertLogs(logging.getLogger(), logging.ERROR):
+            run_async(self.json_failing_async_cache_map.update_status)
+            self.assertEqual(len(self.json_failing_async_cache_map), 0)
+
+    def test_command_failing_update(self):
+        with self.assertLogs(logging.getLogger(), logging.ERROR):
+            run_async(self.json_failing_async_cache_map.update_status)
+            self.assertEqual(len(self.json_failing_async_cache_map), 0)
