166a0d385f8631f57140e1d13c4db0f85855d19c
Manuel Giffels <giffels@gmail.com>
2019-03-13 13:31:18 +0100

    Implementation of HTCondor site adapter deploy_resource

--- tardis/adapters/sites/htcondor.py
+++ tardis/adapters/sites/htcondor.py
@@ -1,9 +1,28 @@
 from ...configuration.configuration import Configuration
 from ...exceptions.tardisexceptions import TardisError
 from ...interfaces.siteadapter import SiteAdapter
+from ...utilities.asynccachemap import AsyncCacheMap
+from ...utilities.attributedict import AttributeDict
+from ...utilities.staticmapping import StaticMapping
 from ...utilities.executors.shellexecutor import ShellExecutor
 
 from contextlib import contextmanager
+from datetime import datetime
+from functools import partial
+
+import re
+
+
+async def htcondor_queue_updater(executor):
+    attributes = dict(Owner="Owner", JobStatus="JobStatus", ClusterId="ClusterId", ProcId="ProcId")
+    attributes_string = " ".join(attributes.values())
+    queue_command = f"condor_q -af:t {attributes_string}"
+
+    htcondor_queue = {}
+
+    executor.run_command(queue_command)
+
+    return htcondor_queue
 
 
 class HTCondorSiteAdapter(SiteAdapter):
@@ -13,8 +32,23 @@ class HTCondorSiteAdapter(SiteAdapter):
         self._site_name = site_name
         self._executor = getattr(self.configuration, 'executor', ShellExecutor())
 
+        key_translator = StaticMapping(resource_id='cluster_id', created='created', updated='updated')
+        translator_functions = StaticMapping(cluster_id=lambda x: int(x))
+
+        self.handle_response = partial(self.handle_response, key_translator=key_translator,
+                                       translator_functions=translator_functions)
+
+        self._htcondor_queue = AsyncCacheMap(update_coroutine=partial(htcondor_queue_updater, self._executor),
+                                             max_age=self.configuration.max_age * 60)
+
     async def deploy_resource(self, resource_attributes):
-        pass
+        submit_command = f"condor_submit {self.configuration.jdl}"
+        response = await self._executor.run_command(submit_command)
+        pattern = re.compile(r"^.*?(?P<jobs>\d+).*?(?P<cluster_id>\d+).$", flags=re.MULTILINE)
+        response = AttributeDict(pattern.search(response.stdout).groupdict())
+        now = datetime.now()
+        response.update(AttributeDict(created=now, updated=now))
+        return self.handle_response(response)
 
     @property
     def machine_meta_data(self):
@@ -29,13 +63,20 @@ class HTCondorSiteAdapter(SiteAdapter):
         return self._site_name
 
     async def resource_status(self, resource_attributes):
-        pass
+        await self._htcondor_queue.update_status()
+        try:
+            self._htcondor_queue[resource_attributes.resource_id]
+        except KeyError:
+            pass
 
     async def stop_resource(self, resource_attributes):
-        pass
+        """"Stopping machines is not supported in HTCondor, therefore terminate is called!"""
+        return await self.terminate_resource(resource_attributes)
 
     async def terminate_resource(self, resource_attributes):
-        pass
+        terminate_command = f"condor_rm {resource_attributes.resource_id}"
+        response = self._executor.run_command(terminate_command)
+        return response
 
     @contextmanager
     def handle_exceptions(self):
