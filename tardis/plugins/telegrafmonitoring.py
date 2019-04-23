728242ff1a10b44f75d0e31d9adf5f9869345e18
Manuel Giffels <giffels@gmail.com>
2019-04-23 16:48:10 +0200

    Add telegraf implementation based on aiotelegraf

--- tardis/plugins/telegrafmonitoring.py
+++ tardis/plugins/telegrafmonitoring.py
@@ -1,6 +1,24 @@
+from ..configuration.configuration import Configuration
 from ..interfaces.plugin import Plugin
+
+import aiotelegraf
 
 
 class TelegrafMonitoring(Plugin):
+    def __init__(self):
+        config = Configuration()
+
+        self.host = config.Plugins.TelegrafMonitoring.host
+        self.port = config.Plugins.TelegrafMonitoring.port
+        self.default_tags = getattr(config.Plugins.TelegrafMonitoring, 'default_tags', None)
+
+        self.client = aiotelegraf.Client(host=config.Plugins.TelegrafMonitoring.host,
+                                         port=config.Plugins.TelegrafMonitoring.port,
+                                         tags=getattr(config.Plugins.TelegrafMonitoring, 'default_tags', None))
+
     async def notify(self, state, resource_attributes):
-        pass
+        await self.client.connect()
+        data = {'state': str(state)}
+        data.update(resource_attributes)
+        self.client.metric('tardis_metric', data)
+        await self.client.close()
