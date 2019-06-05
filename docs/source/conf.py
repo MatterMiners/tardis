999a1f9290cd8c88599a9a97b680faa13323d6b0
Manuel Giffels <giffels@gmail.com>
2019-06-05 22:47:08 +0200

    Change year to 2019

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'tardis'
-copyright = '2018, Manuel Giffels'
+copyright = '2019, Manuel Giffels'
 author = 'Manuel Giffels'
 
 # The short X.Y version
