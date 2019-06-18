dacd1647c7a7bde7dc234f98cbd9a5806069d350
Manuel Giffels <giffels@gmail.com>
2019-06-18 08:59:19 +0200

    Enable draining in FakeBatchSystem adapter

--- tardis/adapters/batchsystems/fakebatchsystem.py
+++ tardis/adapters/batchsystems/fakebatchsystem.py
@@ -6,22 +6,29 @@ from tardis.interfaces.batchsystemadapter import MachineStatus
 class FakeBatchSystemAdapter(BatchSystemAdapter):
     def __init__(self):
         config = Configuration()
-        self.dummy_config = config.BatchSystem
+        self.fake_config = config.BatchSystem
+        self._drained_machines = {}
 
     async def disintegrate_machine(self, drone_uuid):
         return
 
     async def drain_machine(self, drone_uuid):
+        self._drained_machines[drone_uuid] = MachineStatus.Drained
         return
 
     async def integrate_machine(self, drone_uuid):
         return
 
     async def get_allocation(self, drone_uuid):
-        return self.dummy_config.allocation
+        return self.fake_config.allocation
 
     async def get_machine_status(self, drone_uuid):
-        return getattr(MachineStatus, self.dummy_config.machine_status)
+        try:
+            machine_status = self._drained_machines[drone_uuid]
+        except KeyError:
+            return getattr(MachineStatus, self.fake_config.machine_status)
+        else:
+            return machine_status
 
     async def get_utilization(self, drone_uuid):
-        return self.dummy_config.utilization
+        return self.fake_config.utilization
--- tests/adapters_t/batchsystems_t/test_fakebatchsystem.py
+++ tests/adapters_t/batchsystems_t/test_fakebatchsystem.py
@@ -8,6 +8,7 @@ from unittest import TestCase
 
 
 class TestFakeBatchSystemAdapter(TestCase):
+    mock_config_patcher = None
     @classmethod
     def setUpClass(cls):
         cls.mock_config_patcher = patch('tardis.adapters.batchsystems.fakebatchsystem.Configuration')
@@ -23,22 +24,25 @@ class TestFakeBatchSystemAdapter(TestCase):
         config.BatchSystem.utilization = 1.0
         config.BatchSystem.machine_status = "Available"
 
-        self.dummy_adapter = FakeBatchSystemAdapter()
+        self.fake_adapter = FakeBatchSystemAdapter()
 
     def test_disintegrate_machine(self):
-        self.assertIsNone(run_async(self.dummy_adapter.disintegrate_machine, 'test-123'))
+        self.assertIsNone(run_async(self.fake_adapter.disintegrate_machine, 'test-123'))
 
     def test_drain_machine(self):
-        self.assertIsNone(run_async(self.dummy_adapter.drain_machine, 'test-123'))
+        self.assertIsNone(run_async(self.fake_adapter.drain_machine, 'test-123'))
 
     def test_integrate_machine(self):
-        self.assertIsNone(run_async(self.dummy_adapter.integrate_machine, 'test-123'))
+        self.assertIsNone(run_async(self.fake_adapter.integrate_machine, 'test-123'))
 
     def test_get_allocation(self):
-        self.assertEqual(run_async(self.dummy_adapter.get_allocation, 'test-123'), 1.0)
+        self.assertEqual(run_async(self.fake_adapter.get_allocation, 'test-123'), 1.0)
 
     def test_get_machine_status(self):
-        self.assertEqual(run_async(self.dummy_adapter.get_machine_status, 'test-123'), MachineStatus.Available)
+        self.assertEqual(run_async(self.fake_adapter.get_machine_status, 'test-123'), MachineStatus.Available)
+
+        run_async(self.fake_adapter.drain_machine, 'test-123')
+        self.assertEqual(run_async(self.fake_adapter.get_machine_status, 'test-123'), MachineStatus.Drained)
 
     def test_get_utilization(self):
-        self.assertEqual(run_async(self.dummy_adapter.get_utilization, 'test-123'), 1.0)
+        self.assertEqual(run_async(self.fake_adapter.get_utilization, 'test-123'), 1.0)
