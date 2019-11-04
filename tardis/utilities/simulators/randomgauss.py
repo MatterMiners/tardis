127d97e6b03313ebe42ce09f9b7176c35abb75d9
Stefan Kroboth <stefan.kroboth@gmail.com>
2019-11-04 16:10:27 +0100

    documentation of RandomGauss

--- tardis/utilities/simulators/randomgauss.py
+++ tardis/utilities/simulators/randomgauss.py
@@ -6,10 +6,28 @@ import random
 
 @enable_yaml_load("!RandomGauss")
 class RandomGauss(Simulator):
+    """
+    Returns a random number drawn from a Gaussian distribution
+    """
+
     def __init__(self, mu, sigma, seed=None):
+        """
+        :param mu: mean
+        :type mu: float
+        :param sigma: standard deviation
+        :type sigma: float
+        :param seed: random seed
+        :type seed: int
+        """
         self._mu = mu
         self._sigma = sigma
         random.seed(seed)
 
     def get_value(self) -> float:
+        """
+        Returns a random number drawn from a Gaussian distribution
+
+        :return: random number drawn from a Gaussian distribution
+        :rtype: float
+        """
         return random.gauss(self._mu, self._sigma)
