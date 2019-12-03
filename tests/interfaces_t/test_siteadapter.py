ef0ca039d4efbd25c2749fad0f6e17ba5fadd417
Manuel Giffels <giffels@gmail.com>
2019-12-03 17:30:10 +0100

    Add unittest for site adapter interface

---
+++ tests/interfaces_t/test_siteadapter.py
@@ -0,0 +1,89 @@
+from tardis.interfaces.siteadapter import SiteAdapter
+from tardis.utilities.attributedict import AttributeDict
+
+from ..utilities.utilities import run_async
+
+from unittest import TestCase
+from unittest.mock import patch
+
+
+class TestSiteAdapter(TestCase):
+    @patch.multiple(SiteAdapter, __abstractmethods__=set())
+    def setUp(self) -> None:
+        self.site_adapter = SiteAdapter()
+
+    def test_configuration(self):
+        with self.assertRaises(AttributeError):
+            self.site_adapter.configuration
+
+    def test_deploy_resource(self):
+        self.assertEqual(
+            run_async(self.site_adapter.deploy_resource, dict()), NotImplemented
+        )
+
+    def test_handle_exception(self):
+        self.assertEqual(self.site_adapter.handle_exceptions(), NotImplemented)
+
+    def test_handle_response_matching(self):
+        test_response = {"test": 123}
+        test_key_translator = {"new_test": "test"}
+        test_translator_functions = {"test": str}
+        self.assertEqual(
+            self.site_adapter.handle_response(
+                test_response,
+                test_key_translator,
+                test_translator_functions,
+                additional="test123",
+            ),
+            AttributeDict(new_test="123", additional="test123"),
+        )
+
+    def test_handle_response_non_matching_with_additional(self):
+        test_response = {"other_test": 123}
+        test_key_translator = {"new_test": "test"}
+        test_translator_functions = {"test": str}
+
+        self.assertEqual(
+            self.site_adapter.handle_response(
+                test_response,
+                test_key_translator,
+                test_translator_functions,
+                additional="test123",
+            ),
+            AttributeDict(additional="test123"),
+        )
+
+    def test_handle_response_non_matching_wo_additional(self):
+        test_response = {"other_test": 123}
+        test_key_translator = {"new_test": "test"}
+        test_translator_functions = {"test": str}
+
+        self.assertEqual(
+            self.site_adapter.handle_response(
+                test_response, test_key_translator, test_translator_functions
+            ),
+            AttributeDict(),
+        )
+
+    def test_machine_type(self):
+        with self.assertRaises(AttributeError):
+            self.site_adapter.machine_type
+
+    def test_resource_status(self):
+        self.assertEqual(
+            run_async(self.site_adapter.resource_status, dict()), NotImplemented
+        )
+
+    def test_site_name(self):
+        with self.assertRaises(AttributeError):
+            self.site_adapter.site_name
+
+    def test_stop_resource(self):
+        self.assertEqual(
+            run_async(self.site_adapter.stop_resource, dict()), NotImplemented
+        )
+
+    def test_terminate_resource(self):
+        self.assertEqual(
+            run_async(self.site_adapter.terminate_resource, dict()), NotImplemented
+        )
