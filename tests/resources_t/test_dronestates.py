eba7c317251da834f4e3759f9917ae4dcaea0b59
Manuel Giffels <giffels@gmail.com>
2020-01-09 13:55:11 +0100

    Fix unittest for drone minimum life time

--- tests/resources_t/test_dronestates.py
+++ tests/resources_t/test_dronestates.py
@@ -217,6 +217,7 @@ class TestDroneStates(TestCase):
         self.assertEqual(self.drone._supply, 0.0)
 
         # Test draining procedure due to exceeding life time of the drone
+        self.drone.demand = 8.0
         self.drone.drone_minimum_lifetime = 1
         self.drone.state.return_value = AvailableState()
         run_async(self.drone.state.return_value.run, self.drone)
