1771b9ab938b10aa959f20da786db7626499ac10
Stefan Kroboth <stefan.kroboth@gmail.com>
2020-04-09 14:44:16 +0200

    Added Monitoring plugin for ElasticSearch

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
         "cobald>=0.12.0",
         "asyncssh",
         "aiotelegraf",
+        "elasticsearch",
     ],
     extras_require={
         "docs": ["sphinx", "sphinx_rtd_theme", "sphinxcontrib-contentui"],
---
+++ tardis/plugins/elasticsearchmonitoring.py
@@ -0,0 +1,69 @@
+from ..configuration.configuration import Configuration
+from ..interfaces.plugin import Plugin
+from ..interfaces.state import State
+from ..utilities.attributedict import AttributeDict
+
+import logging
+from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import ConflictError
+from time import time
+from datetime import datetime
+
+
+class ElasticsearchMonitoring(Plugin):
+    """
+    The :py:class:`~tardis.plugins.elasticsearchmonitoring.ElasticsearchMonitoring`
+    implements an interface to monitor the state of the Drones using Elasticsearch.
+    """
+
+    def __init__(self):
+        self.logger = logging.getLogger("elasticsearchmonitoring")
+        self.logger.setLevel(logging.DEBUG)
+        config = Configuration().Plugins.ElasticsearchMonitoring
+
+        self._index = config.index + "-" + datetime.now().strftime("%Y-%m-%d")
+        self._host = config.host
+        self._port = config.port
+        self._meta = config.meta
+
+        self._es = Elasticsearch([{"host": self._host, "port": self._port}])
+
+    async def notify(self, state: State, resource_attributes: AttributeDict) -> None:
+        """
+        Pushes drone info at every state change to an ElasticSearch instance.
+
+        :param state: New state of the Drone
+        :type state: State
+        :param resource_attributes: Contains all meta-data of the Drone (created and
+            updated timestamps, dns name, unique id, site_name, machine_type, etc.)
+        :type resource_attributes: AttributeDict
+        :return: None
+        """
+        self.logger.debug(
+            f"Drone: {str(resource_attributes)} has changed state to {state}"
+        )
+
+        # Add state
+        resource_attributes["state"] = str(state)
+        # Add meta
+        resource_attributes["meta"] = self._meta
+        # Add timestamp
+        resource_attributes["timestamp"] = int(time() * 1000)
+        # Make serializable
+        resource_attributes["resource_status"] = str(
+            resource_attributes["resource_status"]
+        )
+
+        revision = 0
+        while True:
+            try:
+                # Add revision number
+                resource_attributes["revision"] = revision
+
+                doc_id = resource_attributes["drone_uuid"] + "-" + str(revision)
+
+                self._es.create(index=self._index, id=doc_id, body=resource_attributes)
+
+                break
+            except ConflictError:
+                revision += 1
