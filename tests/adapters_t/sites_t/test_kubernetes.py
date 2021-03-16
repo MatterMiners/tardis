7addac8884e8b885c834d0b51d2a59415c43b8e5
Manuel Giffels <giffels@gmail.com>
2021-03-16 13:40:16 +0100

    Remove aiohttp warnings from tests

--- tests/adapters_t/sites_t/test_kubernetes.py
+++ tests/adapters_t/sites_t/test_kubernetes.py
@@ -14,6 +14,9 @@ from kubernetes_asyncio import client
 
 
 class TestKubernetesStackAdapter(TestCase):
+    mock_config_patcher = None
+    mock_kubernetes_api_patcher = None
+
     @classmethod
     def setUpClass(cls):
         cls.mock_config_patcher = patch(
@@ -117,7 +120,8 @@ class TestKubernetesStackAdapter(TestCase):
     def tearDown(self):
         self.mock_kubernetes_api.reset_mock()
 
-    def test_deploy_resource(self):
+    @patch("kubernetes_asyncio.client.rest.aiohttp")
+    def test_deploy_resource(self, mocked_aiohttp):
         self.assertEqual(
             run_async(
                 self.kubernetes_adapter.deploy_resource,
@@ -147,7 +151,8 @@ class TestKubernetesStackAdapter(TestCase):
     def test_site_name(self):
         self.assertEqual(self.kubernetes_adapter.site_name, "TestSite")
 
-    def test_resource_status(self):
+    @patch("kubernetes_asyncio.client.rest.aiohttp")
+    def test_resource_status(self, mocked_aiohttp):
         self.assertEqual(
             run_async(
                 self.kubernetes_adapter.resource_status,
@@ -216,7 +221,8 @@ class TestKubernetesStackAdapter(TestCase):
             )
         self.update_read_side_effect(exception=None)
 
-    def test_stop_resource(self):
+    @patch("kubernetes_asyncio.client.rest.aiohttp")
+    def test_stop_resource(self, mocked_aiohttp):
         self.body.metadata.uid = "123456"
         self.body.status = client.V1DeploymentStatus(
             conditions=[
@@ -237,7 +243,8 @@ class TestKubernetesStackAdapter(TestCase):
             name="testsite-089123", namespace="default", body=self.body
         )
 
-    def test_terminate_resource(self):
+    @patch("kubernetes_asyncio.client.rest.aiohttp")
+    def test_terminate_resource(self, mocked_aiohttp):
         run_async(
             self.kubernetes_adapter.terminate_resource,
             resource_attributes=AttributeDict(drone_uuid="testsite-089123"),
