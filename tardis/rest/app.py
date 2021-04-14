22c91e9d7bcc36808a38f39b3f0a813d117ebe6c
Manuel Giffels <giffels@gmail.com>
2021-04-14 12:35:16 +0200

    Add closure to get sqlregistry

--- tardis/rest/app.py
+++ tardis/rest/app.py
@@ -1,14 +1,20 @@
 from . import crud
 from ..plugins.sqliteregistry import SqliteRegistry
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import Depends, FastAPI, HTTPException, Query
 
 app = FastAPI()
 
-sql_registry = SqliteRegistry()
+
+def get_sql_registry():
+    sql_registry = SqliteRegistry()
+    return lambda: sql_registry
 
 
 @app.get("/state/{drone_uuid}")
-async def get_state(drone_uuid: str = Query(..., regex=r"^\S+-[A-Fa-f0-9]{10}$")):
+async def get_state(
+    drone_uuid: str = Query(..., regex=r"^\S+-[A-Fa-f0-9]{10}$"),
+    sql_registry: SqliteRegistry = Depends(get_sql_registry()),
+):
     query_result = await crud.get_resource_state(sql_registry, drone_uuid)
     try:
         query_result = query_result[0]
