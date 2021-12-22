3491a86ae2c4245ddbbaba2825b37810054c2811
Max Fischer <maxfischer2781@gmail.com>
2021-12-22 12:55:18 +0100

    draft for bulk execution

---
+++ tardis/utilities/bulk/execution.py
@@ -0,0 +1,108 @@
+from typing import TypeVar, Generic, Protocol, Iterable, List, Tuple, Optional
+import asyncio
+import time
+import sys
+
+
+T = TypeVar("T")
+R = TypeVar("R")
+
+
+class BulkCommand(Protocol[T, R]):
+    """Protocol of callables suitable for :py:class:`~.BulkExecution`"""
+
+    async def __call__(self, __tasks: Iterable[T]) -> Optional[Iterable[R]]:
+        ...
+
+
+async def _read(queue: asyncio.Queue[T], max_items: int, max_age: float) -> List[T]:
+    """Read at most ``max_items`` items during ``max_age`` seconds from the ``queue``"""
+    results = []
+    deadline = time.monotonic() + max_age
+    while len(results) < max_items and time.monotonic() < deadline:
+        try:
+            if queue.empty():
+                item = await asyncio.wait_for(queue.get(), deadline - time.monotonic())
+            else:
+                item = queue.get_nowait()
+        except asyncio.TimeoutError:
+            break
+        else:
+            results.append(item)
+            queue.task_done()
+    return results
+
+
+class BulkExecution(Generic[T, R]):
+    """
+    Framework for queueing and executing several tasks via bulk commands
+
+    :param command: async callable that executes several tasks
+    :param bulk_size: maximum number of tasks to execute in one bulk
+    :param bulk_delay: maximum age in seconds of tasks before executing them
+    :param concurrent: how often the `command` may be executed at the same time
+
+    .. note::
+
+        If the ``command`` requires additional arguments,
+        wrap it via :py:func:`~functools.partial`.
+    """
+
+    def __init__(
+        self,
+        command: BulkCommand[T, R],
+        bulk_size: int,
+        bulk_delay: float,
+        concurrent: Optional[int] = None,
+    ):
+        self._command = command
+        self._bulk_size = bulk_size
+        self._bulk_delay = bulk_delay
+        # synchronized counter for active commands
+        self._concurrent = asyncio.Semaphore(value=concurrent or sys.maxsize)
+        # queue of outstanding tasks
+        self._queue_ = None
+        # task handling dispatch from queue to command execution
+        self._master_worker: Optional[asyncio.Task] = None
+
+    async def execute(self, task: T) -> R:
+        """Execute a ``task`` in bulk and return the result"""
+        result: asyncio.Future[R] = asyncio.Future()
+        await self._queue.put((task, result))
+        self._ensure_worker()
+        return await result
+
+    @property
+    def _queue(self) -> asyncio.Queue[Tuple[T, asyncio.Future[R]]]:
+        if self._queue_ is None:
+            self._queue_ = asyncio.Queue()
+        return self._queue_
+
+    def _ensure_worker(self):
+        if self._master_worker is None:
+            self._master_worker = asyncio.create_task(self._bulk_dispatch())
+
+    async def _bulk_dispatch(self):
+        while True:
+            await asyncio.sleep(0)
+            tasks, futures = zip(
+                *(await _read(self._queue, self._bulk_size, self._bulk_delay))
+            )
+            if not tasks:
+                continue
+            await self._concurrent.acquire()
+            asyncio.create_task(self._bulk_execute(tasks, futures))
+
+    async def _bulk_execute(self, tasks: List[T], futures: List[asyncio.Future[R]]):
+        try:
+            results = await self._command(tasks)
+        except Exception as task_exception:
+            for future in futures:
+                future.set_exception(task_exception)
+        else:
+            if results is None:
+                results = [None] * len(futures)
+            for future, result in zip(futures, results):
+                future.set_result(result)
+        finally:
+            self._concurrent.release()
