e7449a644d87bf878b9c0778b611ff99515066fc
Max Fischer <maxfischer2781@gmail.com>
2022-01-05 17:16:53 +0100

    explicitly checking exception types

--- tests/utilities_t/test_bulkexecution.py
+++ tests/utilities_t/test_bulkexecution.py
@@ -62,15 +62,15 @@ class TestBulkExecution(TestCase):
         """Test against illegal settings"""
         for wrong_size in (0, -1, 0.5, 2j, "15"):
             with self.subTest(size=wrong_size):
-                with self.assertRaises(Exception):
+                with self.assertRaises(ValueError):
                     BulkExecution(CallCounter(), size=wrong_size, delay=1.0)
         for wrong_delay in (0, -5, 17j, "10"):
             with self.subTest(delay=wrong_delay):
-                with self.assertRaises(Exception):
+                with self.assertRaises((ValueError, TypeError)):
                     BulkExecution(CallCounter(), size=100, delay=wrong_delay)
         for wrong_concurrency in (0, 2.3, -5, 17j, "10"):
             with self.subTest(delay=wrong_concurrency):
-                with self.assertRaises(Exception):
+                with self.assertRaises(ValueError):
                     BulkExecution(
                         CallCounter(),
                         size=100,
