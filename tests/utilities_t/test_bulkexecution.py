f3e18a24690f5d9a1e613d0fb7d67ae513e19184
Max Fischer <maxfischer2781@gmail.com>
2022-01-05 16:55:42 +0100

    bulk tests

---
+++ tests/utilities_t/test_bulkexecution.py
@@ -0,0 +1,59 @@
+import asyncio
+import time
+import sys
+from unittest import TestCase
+
+from tardis.utilities.bulkexecution import BulkExecution
+
+from ..utilities.utilities import run_async
+
+
+class CallCounter:
+    def __init__(self, start=0):
+        self.calls = start
+
+    async def __call__(self, *tasks):
+        this_call = self.calls
+        self.calls += 1
+        # make *some* runs pause so that this isn't a trivially sequential test
+        if this_call % 2:
+            await asyncio.sleep(0)
+        return [(i, this_call) for i in tasks]
+
+
+class TestBulkExecution(TestCase):
+    @staticmethod
+    async def execute(execution: BulkExecution, count: int, delay=None):
+        tasks = []
+        for i in range(count):
+            tasks.append(asyncio.ensure_future(execution(i)))
+            if delay is not None:
+                await asyncio.sleep(delay)
+        return await asyncio.gather(*tasks)
+
+    def test_bulk_size(self):
+        """Test that bulks are formed by size"""
+        for size in (1, 10, 100, 1000, 2, 3, 5, 7, 97, 2129):
+            with self.subTest(size=size):
+                execution = BulkExecution(CallCounter(), size=size, delay=0.1)
+                result = run_async(self.execute, execution, count=size * 3 + 5)
+                self.assertEqual(result, [(i, i // size) for i in range(size * 3 + 5)])
+
+    def test_bulk_delay(self):
+        """Test that bulks are formed by delay"""
+        # check that delay forces a bulk if the size is too large
+        bulk_delay = 0.05
+        execution = BulkExecution(CallCounter(), size=2 ** 32, delay=bulk_delay)
+        before = time.monotonic()
+        result = run_async(self.execute, execution, count=2048)
+        after = time.monotonic()
+        self.assertLess(after - before, bulk_delay * 4)
+        self.assertEqual(result, [(i, 0) for i in range(2048)])
+
+    def test_delay_tiny(self):
+        """Test that a tiny delay cannot stall execution"""
+        # sys.float_info.min is not the smallest float possible,
+        # but it should be insignificant in all math
+        execution = BulkExecution(CallCounter(), size=2 ** 32, delay=sys.float_info.min)
+        result = run_async(self.execute, execution, count=2048)
+        self.assertEqual(result, [(i, i) for i in range(2048)])
