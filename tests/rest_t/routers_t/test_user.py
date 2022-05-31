b69f865c6410e7aa785c2d88d560fb6cc38919e0
Alexander Haas <mail@haas-alexander.com>
2022-05-31 21:39:38 +0200

    feat: added test for all /user functions

--- tests/rest_t/routers_t/test_user.py
+++ tests/rest_t/routers_t/test_user.py
@@ -3,6 +3,7 @@ from tests.utilities.utilities import run_async
 
 # TODO: decrease code repetition by extracting basic auth test into separate function
 
+
 class TestLogin(TestCaseRouters):
     # Reminder: When defining `setUp`, `setUpClass`, `tearDown` and `tearDownClass`
     # in router tests the corresponding super().function() needs to be called as well.
@@ -62,8 +63,57 @@ class TestLogin(TestCaseRouters):
         self.config.Services.restapi.get_user.side_effect = None
 
         self.clear_lru_cache()
-        self.login()
-        )
-        self.login()
+        self.test_user["password"] = "wrong"
+        response = run_async(self.client.post, "/user/login", json=self.test_user)
         self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), {"detail": "Incorrect username or password"})
+
+    def test_logout(self):
+        # Not logged in yet
+        self.clear_lru_cache()
+        response = run_async(self.client.delete, "/user/logout")
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            response.json(), {"detail": "Missing cookie access_token_cookie"}
+        )
+
+        # correct login
         self.login()
+        response = run_async(self.client.delete, "/user/logout")
+        self.assertEqual(response.status_code, 200)
+
+        # prevent second logout
+        response = run_async(self.client.delete, "/user/logout")
+        self.assertEqual(response.status_code, 401)
+
+    def test_refresh(self):
+        # Not logged in yet
+        self.clear_lru_cache()
+        response = run_async(self.client.post, "/user/refresh")
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            response.json(), {"detail": "Missing cookie refresh_token_cookie"}
+        )
+
+        # correct login
+        self.login()
+        response = run_async(self.client.post, "/user/refresh")
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_me(self):
+        # Not logged in yet
+        self.clear_lru_cache()
+        response = run_async(self.client.get, "/user/me")
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            response.json(), {"detail": "Missing cookie access_token_cookie"}
+        )
+
+        # should work
+        self.login()
+        response = run_async(self.client.get, "/user/me")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(), {"user_name": "test", "scopes": ["resources:get"]}
+        )
