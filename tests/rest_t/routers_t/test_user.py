01dfcaf27dac650579bc70990be2ab06c66f99fd
Alexander Haas <mail@haas-alexander.com>
2022-05-31 21:26:10 +0200

    todo: do some refactoring in the future

--- tests/rest_t/routers_t/test_user.py
+++ tests/rest_t/routers_t/test_user.py
@@ -1,6 +1,7 @@
 from tests.rest_t.routers_t.base_test_case_routers import TestCaseRouters
 from tests.utilities.utilities import run_async
 
+# TODO: decrease code repetition by extracting basic auth test into separate function
 
 class TestLogin(TestCaseRouters):
     # Reminder: When defining `setUp`, `setUpClass`, `tearDown` and `tearDownClass`
