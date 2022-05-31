99534a39780bf186178ccdc2598817fe5ff3d767
Alexander Haas <mail@haas-alexander.com>
2022-05-31 09:49:03 +0200

    fix: delete create_access_token tests as they are generated automatically by the jwt package + comment out the authorization tests as it isn't clear if authorization is needed for now

--- tests/rest_t/app_t/test_security.py
+++ tests/rest_t/app_t/test_security.py
@@ -51,88 +51,49 @@ class TestSecurity(TestCase):
         get_algorithm.cache_clear()
         get_user.cache_clear()
 
-    @patch("tardis.rest.app.security.datetime")
-    def test_create_access_token(self, mocked_datetime):
-        self.clear_lru_cache()
+    # def test_check_authorization(self):
+    #     self.clear_lru_cache()
+    #     security_scopes = SecurityScopes(["resources:get"])
+    #     token_data = check_authorization(
+    #         security_scopes, self.infinite_resources_get_token
+    #     )
 
-        token = create_access_token(user_name="test", scopes=["resources:get"])
+    #     self.assertEqual(
+    #         token_data, TokenData(scopes=security_scopes.scopes, user_name="test")
+    #     )
 
-        self.assertEqual(token, self.infinite_resources_get_token)
+    #     security_scopes = SecurityScopes(["resources:put"])
+    #     with self.assertRaises(HTTPException) as he:
+    #         check_authorization(security_scopes, self.infinite_resources_get_token)
+    #     self.assertEqual(he.exception.status_code, status.HTTP_401_UNAUTHORIZED)
+    #     self.assertEqual(he.exception.detail, "Not enough permissions")
 
-        self.clear_lru_cache()
+    #     token_data = check_authorization(
+    #         security_scopes, self.infinite_resources_get_update_token
+    #     )
+    #     self.assertEqual(
+    #         token_data,
+    #         TokenData(scopes=["resources:get", "resources:put"], user_name="test"),
+    #     )
 
-        token = create_access_token(
-            user_name="test",
-            scopes=["resources:get"],
-            secret_key="c2ac5e498f6287c58fa941d0d2cfaf2dc271762a7ba03dcfc3ceb91bb1895d05",  # noqa B950
-            algorithm=self.algorithm,
-        )
+    #     security_scopes = SecurityScopes()
+    #     check_authorization(security_scopes, self.infinite_resources_get_token)
 
-        self.assertEqual(
-            token,
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0Iiwic2NvcGVzIjpbInJlc291cmNlczpnZXQiXX0.bpEZ-l6yC4A7mHV1OjK6WUBeFrjCGMQ7kN9ElXGORe4",  # noqa B950
-        )
+    #     with self.assertRaises(HTTPException) as he:
+    #         check_authorization(security_scopes, "1234567890abdcef")
+    #     self.assertEqual(he.exception.status_code, status.HTTP_401_UNAUTHORIZED)
+    #     self.assertEqual(he.exception.detail, "Could not validate credentials")
 
-        self.clear_lru_cache()
-        mocked_datetime.utcnow.return_value = datetime.utcfromtimestamp(0)
-        token = create_access_token(
-            user_name="test",
-            scopes=["resources:get"],
-            expires_delta=timedelta(minutes=15),
-        )
+    # @patch("tardis.rest.app.security.jwt")
+    # def test_check_authorization_jwt_error(self, mocked_jwt):
+    #     mocked_jwt.decode.side_effect = JWTError
 
-        self.assertEqual(token, self.limited_resources_get_token)
+    #     with self.assertRaises(HTTPException) as he:
+    #         check_authorization(SecurityScopes(), self.infinite_resources_get_token)
+    #     self.assertEqual(he.exception.status_code, status.HTTP_401_UNAUTHORIZED)
+    #     self.assertEqual(he.exception.detail, "Could not validate credentials")
 
-        self.clear_lru_cache()
-        token = create_access_token(
-            user_name="test", scopes=["resources:get", "resources:put"]
-        )
-
-        self.assertEqual(token, self.infinite_resources_get_update_token)
-
-    def test_check_authorization(self):
-        self.clear_lru_cache()
-        security_scopes = SecurityScopes(["resources:get"])
-        token_data = check_authorization(
-            security_scopes, self.infinite_resources_get_token
-        )
-
-        self.assertEqual(
-            token_data, TokenData(scopes=security_scopes.scopes, user_name="test")
-        )
-
-        security_scopes = SecurityScopes(["resources:put"])
-        with self.assertRaises(HTTPException) as he:
-            check_authorization(security_scopes, self.infinite_resources_get_token)
-        self.assertEqual(he.exception.status_code, status.HTTP_401_UNAUTHORIZED)
-        self.assertEqual(he.exception.detail, "Not enough permissions")
-
-        token_data = check_authorization(
-            security_scopes, self.infinite_resources_get_update_token
-        )
-        self.assertEqual(
-            token_data,
-            TokenData(scopes=["resources:get", "resources:put"], user_name="test"),
-        )
-
-        security_scopes = SecurityScopes()
-        check_authorization(security_scopes, self.infinite_resources_get_token)
-
-        with self.assertRaises(HTTPException) as he:
-            check_authorization(security_scopes, "1234567890abdcef")
-        self.assertEqual(he.exception.status_code, status.HTTP_401_UNAUTHORIZED)
-        self.assertEqual(he.exception.detail, "Could not validate credentials")
-
-    @patch("tardis.rest.app.security.jwt")
-    def test_check_authorization_jwt_error(self, mocked_jwt):
-        mocked_jwt.decode.side_effect = JWTError
-
-        with self.assertRaises(HTTPException) as he:
-            check_authorization(SecurityScopes(), self.infinite_resources_get_token)
-        self.assertEqual(he.exception.status_code, status.HTTP_401_UNAUTHORIZED)
-        self.assertEqual(he.exception.detail, "Could not validate credentials")
-
-        mocked_jwt.decode.side_effect = None
+    #     mocked_jwt.decode.side_effect = None
 
     def test_check_authentication(self):
         self.clear_lru_cache()
