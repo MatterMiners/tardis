e2dbabd50c8d48fa5d0f8e7a560c4c24ae37fe77
Alexander Haas <mail@haas-alexander.com>
2022-08-16 16:02:08 +0200

    fix: added type hints in types.py

--- tardis/rest/app/routers/types.py
+++ tardis/rest/app/routers/types.py
@@ -6,6 +6,7 @@ from .. import crud, database
 from ....plugins.sqliteregistry import SqliteRegistry
 from fastapi import APIRouter, Depends, Security
 from ..scopes import Resources
+from fastapi_jwt_auth import AuthJWT
 
 router = APIRouter(prefix="/types", tags=["types", "resources"])
 
@@ -23,7 +24,7 @@ def sql_to_list(query_result: List[Dict]) -> List[str]:
 @router.get("/states", description="Get all available states")
 async def get_resource_state(
     sql_registry: SqliteRegistry = Depends(database.get_sql_registry()),
-    _=Security(security.check_authorization, scopes=[Resources.get]),
+    _: AuthJWT = Security(security.check_authorization, scopes=[Resources.get]),
 ):
     query_result = await crud.get_available_states(sql_registry)
     return sql_to_list(query_result)
@@ -32,7 +33,7 @@ async def get_resource_state(
 @router.get("/sites", description="Get all available sites")
 async def get_resource_sites(
     sql_registry: SqliteRegistry = Depends(database.get_sql_registry()),
-    _=Security(security.check_authorization, scopes=[Resources.get]),
+    _: AuthJWT = Security(security.check_authorization, scopes=[Resources.get]),
 ):
     query_result = await crud.get_available_sites(sql_registry)
     return sql_to_list(query_result)
@@ -41,7 +42,7 @@ async def get_resource_sites(
 @router.get("/machine_types", description="Get all available machine types")
 async def get_resource_types(
     sql_registry: SqliteRegistry = Depends(database.get_sql_registry()),
-    _=Security(security.check_authorization, scopes=[Resources.get]),
+    _: AuthJWT = Security(security.check_authorization, scopes=[Resources.get]),
 ):
     query_result = await crud.get_available_machine_types(sql_registry)
     return sql_to_list(query_result)
