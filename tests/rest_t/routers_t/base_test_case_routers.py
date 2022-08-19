cd700f616e86a382740b76c9e7d5b9f909308ac2
Alexander Haas <mail@haas-alexander.com>
2022-08-19 16:10:04 +0200

    feat: utility functions and fixed scopes

--- tests/rest_t/routers_t/base_test_case_routers.py
+++ tests/rest_t/routers_t/base_test_case_routers.py
@@ -33,14 +33,11 @@ class TestCaseRouters(TestCase):
         cls.mock_config_patcher.stop()
 
     def setUp(self) -> None:
-        algorithm = "HS256"
-
         self.config = self.mock_config.return_value
-        self.config.Services.restapi.algorithm = algorithm
         self.config.Services.restapi.get_user.return_value = AttributeDict(
             user_name="test",
             hashed_password="$2b$12$Gkl8KYNGRMhx4kB0bKJnyuRuzOrx3LZlWf1CReIsDk9HyWoUGBihG",  # noqa B509
-            scopes=["resources:get", "user:get"],
+            scopes=["resources:get", "user:get", "resources:patch"],
         )
 
         from tardis.rest.app.main import (
@@ -52,6 +49,15 @@ class TestCaseRouters(TestCase):
             "user_name": "test1",
             "password": "test",
         }
+
+    def set_scopes(self, scopes: list):
+        self.config.Services.restapi.get_user.return_value.scopes = scopes
+
+    def reset_scopes(self):
+        self.set_scopes(["resources:get", "user:get", "resources:patch"])
+
+    def get_scopes(self):
+        return self.config.Services.restapi.get_user.return_value.scopes
 
     def tearDown(self) -> None:
         run_async(self.client.aclose)
