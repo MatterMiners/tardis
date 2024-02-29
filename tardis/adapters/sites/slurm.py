f8fd06e72ea6ef255df10ca102dbb72c961a4a66
Sebastian Wozniewski <sebastian.wozniewski@uni-goettingen.de>
2024-02-29 12:10:59 +0100

    Revert "use list comprehension instead of loop"
    This reverts commit 1c9cd849328af1db1cb8f9dcdeebb6fd94184048.

--- tardis/adapters/sites/slurm.py
+++ tardis/adapters/sites/slurm.py
@@ -60,9 +60,10 @@ async def squeue(
     else:
         # Not all lines returned in slurm_status.stdout belong to the actual squeue output
         # Only keep lines containing the delimiter
-        cleaned_stdout = [
-            row for row in slurm_status.stdout.splitlines() if "|" in row
-        ]
+        cleaned_stdout = []
+        for row in slurm_status.stdout.splitlines():
+            if "|" in row:
+                cleaned_stdout.append(row)
         for row in csv_parser(
             "\n".join(cleaned_stdout), fieldnames=tuple(attributes.keys()), delimiter="|"
         ):
