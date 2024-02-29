e7c31d453200086838dfc91b43455082f30bc3ae
Manuel Giffels <giffels@gmail.com>
2024-02-29 16:24:01 +0100

    Bump release version to 0.8.2

--- tardis/__about__.py
+++ tardis/__about__.py
@@ -3,8 +3,8 @@ __package__ = "cobald-tardis"
 __summary__ = "Transparent Adaptive Resource Dynamic Integration System"
 __url__ = "https://github.com/matterminers/tardis"
 
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 __author__ = "Manuel Giffels, Matthias Schnepf"
 __email__ = "giffels@gmail.com"
-__copyright__ = "2018 - 2023 %s and Contributors" % __author__
+__copyright__ = "2018 - 2024 %s and Contributors" % __author__
 __keywords__ = "asyncio tardis cloud scheduler"
