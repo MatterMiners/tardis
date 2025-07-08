8ad74152a48e55f8706d416b6b98900bdfa38b6a
Manuel Giffels <giffels@gmail.com>
2025-07-08 08:21:07 +0200

    Replace deprecated regex parameter

--- tardis/rest/app/routers/resources.py
+++ tardis/rest/app/routers/resources.py
@@ -9,7 +9,7 @@ router = APIRouter(prefix="/resources", tags=["resources"])
 
 @router.get("/{drone_uuid}/state", description="Get current state of a resource")
 async def get_resource_state(
-    drone_uuid: str = Path(..., regex=r"^\S+-[A-Fa-f0-9]{10}$"),
+    drone_uuid: str = Path(..., pattern=r"^\S+-[A-Fa-f0-9]{10}$"),
     sql_registry: SqliteRegistry = Depends(database.get_sql_registry()),
     _: AuthJWT = Security(security.check_authorization, scopes=[Resources.get]),
 ):
@@ -34,7 +34,7 @@ async def get_resources(
 
 @router.patch("/{drone_uuid}/drain", description="Gently shut shown drone")
 async def drain_drone(
-    drone_uuid: str = Path(..., regex=r"^\S+-[A-Fa-f0-9]{10}$"),
+    drone_uuid: str = Path(..., pattern=r"^\S+-[A-Fa-f0-9]{10}$"),
     sql_registry: SqliteRegistry = Depends(database.get_sql_registry()),
     _: AuthJWT = Security(security.check_authorization, scopes=[Resources.patch]),
 ):
